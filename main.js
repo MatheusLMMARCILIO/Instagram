05052ed8495b6aecd0ca554f58ba506e2991c8ec
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-05-12 01:07:24 -0300

    12/05/24 01H07

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ const user = "hello"
 
 btnEnter.addEventListener('click', () => {
 
-    const wrongPassword = document.querySelector('.TryAgain')
+    // const wrongPassword = document.querySelector('.TryAgain')
 
-    const nameUser = document.querySelector('.NameUserHere').value
-    const passwordUser = document.querySelector('.passwordInput').value
+    // const nameUser = document.querySelector('.NameUserHere').value
+    // const passwordUser = document.querySelector('.passwordInput').value
 
-    const password = '123'
+    // const password = '123'
 
-    if (nameUser && passwordUser == password && user) {
+    // if (nameUser && passwordUser == password && user) {
        window.location.href = '/pageHome/index.html'
-    } else {
-      wrongPassword.innerHTML = "Senha incorreta!"
-    }
+    // } else {
+    //   wrongPassword.innerHTML = "Senha incorreta!"
+    // }
 
 }
 
